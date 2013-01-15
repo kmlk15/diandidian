42fd1e698b52ad1865c9b0f11a10ff2f19aa2697
markliu2013 <markliu2013@gmail.com>
2013-01-16 00:28:06 +0800

    planning2.html planning function in 1 page

--- html/js/planning.js
+++ html/js/planning.js
@@ -6,13 +6,6 @@ $(function() {
 		var height = $(this).height()-space;
 		$("#attraction-planning-wrap").height(height);
 		$("#plan-attractions-list-wrap, #plan-timeline").height(height-$("#plan-attractions .header").height());
-		
-		//center plan-attractions-list
-		//var wrapWidth = $("#plan-attractions-list-wrap").width();
-		//var rowCount = Math.floor(wrapWidth / planItemWidth);
-		//var mainWidth = planItemWidth*rowCount;
-		//$("#plan-attractions-list").width(mainWidth);
-		
 		if($(this).height()>$("#container").height()) {
 			$("#shadow-divide").height($(this).height());
 		}
@@ -26,13 +19,11 @@ $(function() {
 			$hoverCover.show();
 			$hoverCover.children(".note-input").hide();
 		},
-		function() {}
+		null
 	);
 	
 	$("#plan-attractions-list ul li").hover(
-		function() {
-			
-		},
+		null,
 		function() {
 			$(this).children(".hover-cover").hide();
 		}
@@ -61,10 +52,43 @@ $(function() {
 		{
 			changeYear: true,
 			yearRange: "-0:+3",
-			changeMonth: true
+			changeMonth: true,
+			onSelect: function(dateText, inst) {
+				var startDate = $("#attraction-planning-wrap .start-date .datepicker").datepicker("getDate");
+				var endDate = $(this).datepicker("getDate");
+				if (endDate<startDate) {
+					return;
+				}
+				var durationDay = (endDate.getTime()-startDate.getTime())/(24*60*60*1000);
+				var timeLinkHtml = '<div class="date-line clearfix"><img src="images/time-line-mark.png" width="8" height="11" /><a class="no-sign" href="no-assign">尚未安排</a>';
+				var startLoopDate = startDate;
+				var year = startLoopDate.getFullYear();
+				var month = startLoopDate.getMonth()+1;
+				var date = startLoopDate.getDate();
+				var loopTimeHtml = '<a class="year" href="'+year+'">'+year+'</a><a href="'+year+'/'+month+'/'+date+'">'+month+'月'+date+'日</a>';
+				for(var i=0; i<durationDay; i++) {
+					startLoopDate = new Date(startLoopDate.getTime()+(24*60*60*1000));
+					if (year != startLoopDate.getFullYear()) {
+						year = startLoopDate.getFullYear();
+						loopTimeHtml = loopTimeHtml + '<a class="year" href="'+year+'">'+year+'</a>';
+					}
+					date = startLoopDate.getDate();
+					if (month != (startLoopDate.getMonth()+1)) {
+						month = startLoopDate.getMonth()+1;
+						loopTimeHtml = loopTimeHtml + '<a href="'+year+'/'+month+'/'+date+'">'+month+'月'+date+'日</a>';
+					} else {
+						loopTimeHtml = loopTimeHtml + '<a href="'+year+'/'+month+'/'+date+'">'+date+'日</a>';
+					}
+				}
+				timeLinkHtml = timeLinkHtml + loopTimeHtml + '</div>';
+				$("#plan-date-input p").remove();
+				$("#plan-timeline .date-line").remove();
+				$("#plan-date-input").after(timeLinkHtml);
+				$("#attraction-planning-wrap .date").hide();
+				initialTimeLineLink();
+			}
 		}
 	);
-	//$("#attraction-planning-wrap .end-date > .ui-datepicker").prepend("<h4>结束日期</h4>");
 	
 	$("#plan-date-input .calendar-icon").click(function(e) {
 		$("#attraction-planning-wrap .date").toggle();
@@ -85,7 +109,92 @@ $(function() {
 			}
 		}
 	);
+	initialTimeLineLink();
+	$("#plan-attractions-list-wrap").bind("jsp-scroll-y",function(event, scrollPositionY, isAtTop, isAtBottom) {
+					console.log('Handle jsp-scroll-y',
+								'scrollPositionY=', scrollPositionY);
+	})
 	
+	
+	initialPlanNameLabel();
+	
+		
+})
+
+function findDropHeaderPos(date) {
+	var dateValue = parseInt(date);
+	var returnObj = null;
+	$("#plan-attractions-list h3").each(function(index, element) {
+		var headerClass = $(element).attr("class");
+		if (headerClass) {
+			var classDateValue = parseInt(headerClass.replace("t-", ""));
+			if(classDateValue > dateValue) {
+				returnObj = $(element);
+				return (false);
+			}
+		}
+	});
+	return returnObj;
+}
+
+function setPlanAttractionsListPaddingBottom() {
+	var visibleHeight = $("#plan-attractions-list-wrap").height();
+	var h3Height = $("#plan-attractions-list h3").outerHeight(true);
+	var lastUlHeight = $("#plan-attractions-list ul:last").outerHeight(true);
+	var paddintBottom = visibleHeight - h3Height - lastUlHeight;
+	$("#plan-attractions-list").css("padding-bottom", paddintBottom+"px");
+}
+
+
+function addDroppable() {
+	$("#plan-attractions-list ul").droppable(
+		{
+			drop: function(event, ui) {
+				var $droppingUl = ui.draggable.parent("ul");
+				ui.draggable.appendTo($(this));
+				if ($droppingUl.find("li").length<=0) {
+					$droppingUl.prev("h3").remove();
+					$droppingUl.prev("hr").remove();
+					$droppingUl.remove();
+				}
+			}
+		}
+	)
+}
+
+function initialPlanNameLabel() {
+	var $input = $("#plan-attractions .header .plan-name input");
+	if($input.val() != "") {
+		$input.next("label").hide();
+	}
+	$input.focus(function(e) {
+		$(this).next("label").hide();
+	}).blur(function(e) {
+		if($(this).val()=="") {
+			$(this).next("label").show();
+		}
+	});
+}
+
+function initialTimeLineLink() {
+	var attractionsApi = $("#plan-attractions-list-wrap").data('jsp');
+	$("#plan-timeline .date-line a").click(function(e) {
+		var href = $(this).attr("href");
+		href = href.replace(new RegExp("/", "g") ,"");
+		var $target = $("#plan-attractions-list").find(".t-"+href);
+		if ($target.length>0) {
+			var arrowTopPosstion = 3;
+			var linkHeight = $("#plan-timeline .date-line a").outerHeight(true)+0;
+			arrowTopPosstion = arrowTopPosstion + linkHeight * ($(this).index()-1);
+			$("#plan-timeline .date-line > img").animate({top: arrowTopPosstion+"px"},1000);
+			attractionsApi.scrollToElement($target,true, 1000);
+			$("#plan-timeline .date-line a.active").removeClass("active");
+			$(this).addClass("active");
+			$("#plan-attractions-list h3.active").removeClass("active");
+			$target.addClass("active");
+		}
+		return false;
+	});
 	$("#plan-timeline .date-line a:not('.year')").droppable(
 		{
 		 	hoverClass: 'drop-hover',
@@ -133,76 +242,7 @@ $(function() {
 			}
 		}
 	);
-	
-	var attractionsApi = $("#plan-attractions-list-wrap").data('jsp');
-	$("#plan-timeline .date-line a").click(function(e) {
-		
-		var href = $(this).attr("href");
-		href = href.replace(new RegExp("/", "g") ,"");
-		var $target = $("#plan-attractions-list").find(".t-"+href);
-		if ($target.length>0) {
-			var arrowTopPosstion = 3;
-			var linkHeight = $("#plan-timeline .date-line a").outerHeight(true)+0;
-			arrowTopPosstion = arrowTopPosstion + linkHeight * ($(this).index()-1);
-			$("#plan-timeline .date-line > img").animate({top: arrowTopPosstion+"px"},1000);
-			attractionsApi.scrollToElement($target,true, 1000);
-			$("#plan-timeline .date-line a.active").removeClass("active");
-			$(this).addClass("active");
-			$("#plan-attractions-list h3.active").removeClass("active");
-			$target.addClass("active");
-		}
-		return false;
-	});
-	
-	$("#plan-attractions-list-wrap").bind("jsp-scroll-y",function(event, scrollPositionY, isAtTop, isAtBottom) {
-					console.log('Handle jsp-scroll-y',
-								'scrollPositionY=', scrollPositionY);
-	})
-		
-})
-
-function findDropHeaderPos(date) {
-	var dateValue = parseInt(date);
-	var returnObj = null;
-	$("#plan-attractions-list h3").each(function(index, element) {
-		var headerClass = $(element).attr("class");
-		if (headerClass) {
-			var classDateValue = parseInt(headerClass.replace("t-", ""));
-			if(classDateValue > dateValue) {
-				returnObj = $(element);
-				return (false);
-			}
-		}
-	});
-	return returnObj;
 }
-
-function setPlanAttractionsListPaddingBottom() {
-	var visibleHeight = $("#plan-attractions-list-wrap").height();
-	var h3Height = $("#plan-attractions-list h3").outerHeight(true);
-	var lastUlHeight = $("#plan-attractions-list ul:last").outerHeight(true);
-	var paddintBottom = visibleHeight - h3Height - lastUlHeight;
-	$("#plan-attractions-list").css("padding-bottom", paddintBottom+"px");
-}
-
-
-function addDroppable() {
-	$("#plan-attractions-list ul").droppable(
-		{
-			drop: function(event, ui) {
-				var $droppingUl = ui.draggable.parent("ul");
-				ui.draggable.appendTo($(this));
-				if ($droppingUl.find("li").length<=0) {
-					$droppingUl.prev("h3").remove();
-					$droppingUl.prev("hr").remove();
-					$droppingUl.remove();
-				}
-			}
-		}
-	)
-}
-
-
 
 
 
