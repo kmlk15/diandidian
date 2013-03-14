9e4aed5c15b5652da4977456701aedea2134b5dc
Terence Lui <terence@tmdstudio.com>
2013-03-14 00:53:39 -0700

    added images and json file

---
+++ html/json/json.js
@@ -0,0 +1,158 @@
+[
+{
+	"name":"Hong Kong Convention and Exhibition Centre",
+	"addresses": {
+		"street":"1 Harbour Rd",
+		"district":"Wan Chai",
+		"city":"Hong Kong",
+		"stateProvince":"SARs",
+		"country":"China",
+		"latitude": 100,
+		"longitude": 100
+	},
+	"admissions": {
+		"general": "Free",
+		"currency":"HKD"
+	},
+	"categories":{
+		"level_1":"Architecture",
+		"level_2":"Skyscraper"
+	},
+	"hours":{
+		"monday" :"",
+		"tuesday" :"",
+		"wednesday" :"",
+		"thursday" :"",
+		"friday" :"",
+		"saturday":"",
+		"sunday":"",
+		"holiday":""
+	},
+	"phones":{
+		"General":"(852) 25374591"
+	},
+	"url":"",
+	"pictures":{
+		"result":"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/result_1.jpg",
+		"planning":"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/plan_1.jpg",
+		"hero":[
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/hero_1.jpg", 
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/hero_2.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/hero_3.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/hero_4.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/hero_5.jpg"
+		],
+		"thumbnail":[
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/thb_1.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/thb_2.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/thb_3.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/thb_4.jpg",
+			"/public/images/china/sars/hong_kong/wan_chai/hk_convention_center/thb_5.jpg"
+		]
+	},
+	"fact":"The Hong Kong Convention and Exhibition Centre is one of the two major convention and exhibition venues in Hong Kong, along with AsiaWorld-Expo. It is located in Wan Chai North, Hong Kong Island. Built along the Victoria Harbour, it is linked by covered walkways to nearby hotels and commercial buildings. The venue was designed by Skidmore, Owings and Merrill LLP, in association with Wong and Ouyang Ltd. Larry Oltmanns led the design as the Design Partner with SOM."
+}
+]
+[
+{
+	"name":"Hong Kong Park",
+	"addresses": {
+		"street":"19 Cotton Tree Drive",
+		"district":"Central and Western",
+		"city":"Hong Kong",
+		"stateProvince":"SARs",
+		"country":"China",
+		"latitude": 100,
+		"longitude": 100
+	},
+	"admissions": {
+		"general": "Free",
+		"currency":"HKD"
+	},
+	"categories":{
+		"level_1":"Park",
+		"level_2":"Garden Park"
+	},
+	"hours":{
+		"general" :"Open Daily: 6:00 a.m. to 11:00 p.m."
+	},
+	"phones":{
+		"General":"(852) 25215041",
+		"Fax":"(852) 25215055"
+	},
+	"url":"www.oceanpark.com.hk",
+	"pictures":{
+		"result":"/public/images/china/sars/hong_kong/central_western/hong_kong_park/result_1.jpg",
+		"planning":"/public/images/china/sars/hong_kong/central_western/hong_kong_park/plan_1.jpg",
+		"hero":[
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_1.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_2.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_3.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_4.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_5.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_6.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_7.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/hero_8.jpg"
+		],
+		"thumbnail":[
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_1.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_2.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_3.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_4.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_5.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_6.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_7.jpg",
+			"/public/images/china/sars/hong_kong/central_western/hong_kong_park/thb_8.jpg"
+		]
+	},
+	"fact":"The Hong Kong Park is a public park next to Cotton Tree Drive in Central, Hong Kong. Built at a cost of HK$398 million and opened in May 1991, it covers an area of 80,000 m² and is an example of modern design and facilities blending with natural landscape."
+}
+]
+[
+{
+	"name":"Madame Tussauds Hong Kong",
+	"addresses": {
+		"street":"Shop P101, The Peak Tower, No. 128 Peak Road, The Peak",
+		"district":"Central and Western",
+		"city":"Hong Kong",
+		"stateProvince":"SARs",
+		"country":"China",
+		"latitude": 100,
+		"longitude": 100
+	},
+	"admissions": {
+		"general": "Package includes entrance to Madame Tussauds Hong Kong and Peak Tram two-way ticket.",
+		"adults": 230,
+		"children": "",
+		"students": 138,
+		"seniors": 138,
+		"currency":"HKD"
+	},
+	"categories":{
+		"level_1":"Museum",
+		"level_2":"Tussauds Museum"
+	},
+	"hours":{
+		"general" :"Open Daily: 10am to 10pm"
+	},
+	"phones":{
+		"General":"(852) 28490668"
+	},
+	"url":"http://www.madametussauds.com/HongKong/Default.aspx",
+	"pictures":{
+		"result":"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/result_1.jpg",
+		"planning":"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/plan_1.jpg",
+		"hero":[
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/hero_1.jpg",
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/hero_2.jpg",
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/hero_3.jpg"
+		],
+		"thumbnail":[
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/thb_1.jpg",
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/thb_2.jpg",
+			"/public/images/china/sars/hong_kong/central_western/madame_tussauds_hong_kong/thb_3.jpg"
+		]
+	},
+	"fact":"Madame Tussauds Hong Kong, part of the renowned chain of wax museums founded by Marie Tussaud of France, is located at the Peak Tower on Hong Kong Island in Hong Kong. It is the first and one of the only two permanent Madame Tussauds museums in Asia, the other being the Shanghai branch, which opened in 2006. The Hong Kong branch houses nearly 100 wax figures of internationally-known personalities, with Asian figures taking up more than a third of the total, of which sixteen were Hong Kongers. The wax figures are featured in a range of themed settings such as Hong Kong Glamour, Music Icons, Historical and National Heroes, The Champions and World Premiere."
+}
+]
