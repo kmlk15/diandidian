367f57474f8cdd1dd9a8dcad9a4dc057317d1d7c
wuhao <hzwuhao8@gmail.com>
2013-09-05 23:11:44 +0800

    转换本地数据

--- test/models/JsonMap.scala
+++ test/models/JsonMap.scala
@@ -8,16 +8,18 @@ import play.api.libs.json._
 class JsonMap extends FunSuite {
   val log = LoggerFactory.getLogger(classOf[JsonMap])
 
-  val str="""{ "_id" : "520757c0e4b0ba2a6baf92e4", "imgsrc" : "3e29fab6a2ab0b4efb757025.jpg" }
-{ "_id" : "520757fae4b0ba2a6baf92e6", "imgsrc" : "5e29fab6a2ab0b4e9f757025.jpg" }
-{ "_id" : "520755d40364282014c45533", "imgsrc" : "43554c410282463071657025.jpg" }
-{ "_id" : "520e296703649d3aaacda0a7", "imgsrc" : "6a0adcaaa3d946308592e025.jpg" }
-{ "_id" : "520e33d00364cc9760d2c2ad", "imgsrc" : "ca2c2d0679cc46309b33e025.jpg" }
-{ "_id" : "521aff68e4b0e138f3285353", "imgsrc" : "2535823f831e0b4e56ffa125.jpg" }
-{ "_id" : "521c3618e4b0441b962bdc6d", "imgsrc" : "c6cdb269b1440b4e6163c125.jpg" }
-{ "_id" : "520e291703649d3aaacda0a5", "imgsrc" : "4a0adcaaa3d94630cf82e025.jpg" }
+  val str="""{ "_id" : "520755d40364282014c45533", "imgsrc" : "43554c410282463071657025.jpg" }
+{ "_id" : "52107cd50364599001b2d14b", "imgsrc" : "a41d2b1009954630c5c70125.jpg" }
+{ "_id" : "5210808d0364599001b2d14d", "imgsrc" : "c41d2b100995463077080125.jpg" }
+{ "_id" : "520e34e30364cc9760d2c2b1", "imgsrc" : "0b2c2d0679cc4630cc43e025.jpg" }
+{ "_id" : "521b40aa03643ac6c2a61dae", "imgsrc" : "dad16a2c6ca346306604b125.jpg" }
+{ "_id" : "521b415103643ac6c2a61db0", "imgsrc" : "fad16a2c6ca346300414b125.jpg" }
+{ "_id" : "521b41c103643ac6c2a61db2", "imgsrc" : "1bd16a2c6ca346300b14b125.jpg" }
 { "_id" : "520e33a30364cc9760d2c2ab", "imgsrc" : "aa2c2d0679cc4630b833e025.jpg" }
-{ "_id" : "520e34e30364cc9760d2c2b1", "imgsrc" : "0b2c2d0679cc4630cc43e025.jpg" }"""
+{ "_id" : "520e33d00364cc9760d2c2ad", "imgsrc" : "ca2c2d0679cc46309b33e025.jpg" }
+{ "_id" : "520e291703649d3aaacda0a5", "imgsrc" : "4a0adcaaa3d94630cf82e025.jpg" }
+{ "_id" : "520e296703649d3aaacda0a7", "imgsrc" : "6a0adcaaa3d946308592e025.jpg" }
+{ "_id" : "52209c450364ac1e2b3368fc", "imgsrc" : "bf8633b2e1ca463003c90225.jpg" }"""
   
     
     
