609fabd32f3ccd6e907b40755e4efb351b9e121c
wuhao <hzwuhao8@gmail.com>
2013-09-29 10:11:46 +0800

    增加 console.log 的定义

--- app/assets/javascripts/ie.js
+++ app/assets/javascripts/ie.js
@@ -18,15 +18,5 @@ $(function() {
 	
 })
 
-   var alertFallback = true;
-   if (typeof console === "undefined" || typeof console.log === "undefined") {
-     console = {};
-     if (alertFallback) {
-         console.log = function(msg) {
-              alert(msg);
-         };
-     } else {
-         console.log = function() {};
-     }
-   }
+
 
--- app/assets/javascripts/master.js
+++ app/assets/javascripts/master.js
@@ -1,3 +1,15 @@
+   var alertFallback = false ;
+   if (typeof console === "undefined" || typeof console.log === "undefined") {
+     console = {};
+     if (alertFallback) {
+         console.log = function(msg) {
+              alert(msg);
+         };
+     } else {
+         console.log = function() {};
+     }
+   }
+   
 $(function() {
 	
 	var $win = $(window);
