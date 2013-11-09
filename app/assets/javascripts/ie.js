49d1d3cb0b58767a9771251604a156bdb7215c62
wuhao <hzwuhao8@gmail.com>
2013-11-09 19:53:26 +0800

    头像 CSS ； Planning page  不再需要 通过   ie.js 脚本 进行 宽度 调整

--- app/assets/javascripts/ie.js
+++ app/assets/javascripts/ie.js
@@ -1,8 +1,8 @@
 // JavaScript Document
 $(function() {
 	$("#top-nav .dropdown ul").width($("#top-nav .dropdown").width());
-	$("#user-sign.signin .account .user-info").width($("#user-sign.signin .account .user-info span").width());
-	$("#user-sign.signin .account ul").width($("#user-sign.signin .account").width());
+	//$("#user-sign.signin .account .user-info").width($("#user-sign.signin .account .user-info span").width());
+	//$("#user-sign.signin .account ul").width($("#user-sign.signin .account").width());
 	$("#attraction-detal .thumbnails .ul-wrap ul li img").css("opacity", 0.45);
 	$("#attraction-detal .thumbnails .ul-wrap ul li.active img").css("opacity", 1);
 	$("#attraction-detal .thumbnails .ul-wrap ul li").hover(
